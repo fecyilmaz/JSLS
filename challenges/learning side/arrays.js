14303a3a660d2a4395a4af4576d8f4b5995dfedc
fecyilmaz <fecyilmazz@gmail.com>
2024-08-12 21:30:12 +0300

    FECY-01 learn: arrays

---
+++ challenges/learning side/arrays.js
@@ -0,0 +1,35 @@
+// // ? arrays, functions and objects
+// // ? arrays [], 0 index based
+
+// const friend1 = "papa";
+// const friend2 = "papa";
+// const friend3 = "papa";
+// const friend4 = "papa";
+
+// //! we can store any type of value
+// const friends = ["can","emre","defne","sevi",45,undefined,null];
+
+// console.log(friends[5]);
+// console.log(friends[0]);
+
+// let element;
+// //TODO: there is a more than this method for if condition
+// for (let i = 0; i < friends.length; i++) {
+//     element = friends[i];
+//     console.log(element);
+// }
+
+// console.log(friends.length);
+
+const fruits = ["pineapple","cherry","banana","apple",45];
+let fruit = fruits[0];
+fruits[fruits.length - 1] = "strawberry";
+for (let a = 0; a < fruits.length; a++) {
+    let element = fruits[a];
+    console.log((1+a) + "\t" + element);  
+}
+console.log("----------------------");
+console.log(fruits[0]);
+
+
+
---
+++ challenges/learning side/data.js
@@ -0,0 +1,14 @@
+let a;
+a=5;
+let b;
+b='hello motherfucker';
+console.log(a,typeof(b));
+let value1 = true;
+let value2 = false;
+console.log(value1,typeof(value2));
+const result = null;
+console.log(typeof(result),result);
+let text;
+console.log(typeof text);
+
+
